a0ffc59ba59cd77540a51f1aedb0a8d258dddb90
Michal Kowol <michal.kowol@gmail.com>
2015-10-07 21:59:48 +0200

    NT. Imporved gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,73 @@
-(function () {
-  'use strict';
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var browserify = require('browserify');
+var connect = require('gulp-connect');
+var rt = require('gulp-react-templates');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-  var gulp = require('gulp');
-  var babel = require('gulp-babel');
-  var browserify = require('browserify');
-  var connect = require('gulp-connect');
-  var rt = require('gulp-react-templates');
+var paths = {
+  scripts: 'app/js/**/*.js',
+  ractTemplates: 'app/js/**/*.rt',
+  htmls: ['app/index.html']
+};
 
-  gulp.task('js', function () {
-    return gulp.src('app/js/**/*.js')
-      .pipe(babel())
-      .pipe(gulp.dest('dist/js'))
-      .pipe(connect.reload());
+gulp.task('clean-dist', function() {
+  return del(['dist']);
+});
+
+gulp.task('clean-bower', function() {
+  return del(['app/bower_components']);
+});
+
+gulp.task('js', function () {
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(connect.reload());
+});
+
+gulp.task('rt', function() {
+  return gulp.src(paths.ractTemplates)
+    .pipe(sourcemaps.init())
+    .pipe(rt({modules: 'none'}))
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  return gulp.src(paths.htmls)
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('bower', function () {
+  return gulp.src('app/bower_components/**/*')
+    .pipe(gulp.dest('dist/bower_components'));
+});
+
+gulp.task('connect', function () {
+  return connect.server({
+    root: 'dist',
+    port: 8080,
+    livereload: true
   });
+});
 
-  gulp.task('rt', function() {
-    return gulp.src('app/js/**/*.rt')
-      .pipe(rt({modules: 'none'}))
-      .pipe(babel())
-      .pipe(gulp.dest('dist/js'))
-      .pipe(connect.reload());
-  });
+gulp.task('watch', function () {
+  gulp.watch(paths.htmls, ['html']);
+  gulp.watch(paths.scripts, ['js']);
+  gulp.watch(paths.ractTemplates, ['rt']);
+});
 
-  gulp.task('html', function () {
-    return gulp.src(['app/index.html'])
-      .pipe(gulp.dest('dist'))
-      .pipe(connect.reload());
-  });
-
-  gulp.task('bower', function () {
-    return gulp.src(['app/bower_components/**/*'])
-      .pipe(gulp.dest('dist/bower_components'));
-  });
-
-  gulp.task('connect', function () {
-    return connect.server({
-      root: 'dist',
-      port: 8080,
-      livereload: true
-    });
-  });
-
-  gulp.task('watch', function () {
-    gulp.watch(['app/index.html'], ['html']);
-    gulp.watch(['app/js/**/*.js'], ['js']);
-    gulp.watch(['app/js/**/*.rt'], ['rt']);
-  });
-
-  gulp.task('default', ['bower', 'js', 'html', 'rt', 'connect', 'watch']);
-})();
+gulp.task('build', ['bower', 'js', 'html', 'rt']);
+gulp.task('dist', function (callback) {
+  runSequence('clean-dist', 'build', 'clean-bower', callback);
+});
+gulp.task('server', ['build', 'connect', 'watch']);
+gulp.task('default', ['server']);
